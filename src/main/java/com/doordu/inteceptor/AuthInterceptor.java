e8acf868a1b463f996463ac3562fe9c693fe4631
mazhaoyi <690196003@qq.com>
2017-11-01 10:38:11 +0800

    添加拦截器

---
+++ src/main/java/com/doordu/inteceptor/AuthInterceptor.java
@@ -0,0 +1,31 @@
+package com.doordu.inteceptor;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 测试request
+ */
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,15 +1,28 @@
 package test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 import com.alibaba.fastjson.JSON;
+import com.doordu.controller.vo.UserVo;
 
 public class Test {
 	public static void main(String[] args) {
 //		List<Integer> list = Arrays.asList(1, 2, 3);
-		String strs = "[1, 2, 3]";
-		List<Integer> list = JSON.parseArray(strs, Integer.class);
-		System.out.println(list);
+//		String strs = "[1, 2, 3]";
+//		List<Integer> list = JSON.parseArray(strs, Integer.class);
+//		System.out.println(list);
+		List<UserVo> users = new ArrayList<>(1);
+		UserVo uv = new UserVo();
+		uv.setAge(2);
+		uv.setBirthday(Calendar.getInstance().getTime());
+		uv.setProperties(null);
+		uv.setName("王五");
+		uv.setIsValid(true);
+		uv.setCreateTime(Calendar.getInstance().getTime());
+		users.add(uv);
+		System.out.println(JSON.toJSONString(users));
 	}
 }
